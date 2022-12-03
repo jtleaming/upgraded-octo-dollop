367e5642d54265fa9f008b9b4fc4b3d8a12c06e8
Justin Leaming <jleaming@splashtrack.com>
2022-12-02 17:01:45 -0700

    init

---
+++ Program.cs
@@ -0,0 +1,101 @@
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+
+builder.Services.AddScoped<IServiceWrapperFactory, ServiceWrapperFactory>();
+builder.Services.AddScoped<ISplashTrackServicesWrapper<MyThirdService>, SplashTrackServicesWrapper<MyThirdService>>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
+
+public interface IServiceWrapper
+{
+    string DevEndpoint { get; }
+    string IntEndpoint { get;  }
+    string ProdEndpoint { get;  }
+
+    string Endpoint => $"{RunEnvironment.GetEnvironment() switch {"prod" => ProdEndpoint, "dev" => DevEndpoint, "int" => IntEndpoint}}";
+}
+
+public struct MyService : IServiceWrapper
+{
+    public string DevEndpoint => "https://dev.com";
+    public string IntEndpoint => "https://int.com";
+    public string ProdEndpoint => "https://prod.com";
+
+    public string MyData(string clientId) => $"/api/v1/{clientId}";
+}
+
+public struct MySecondService : IServiceWrapper
+{
+    public string DevEndpoint => "https://dev.com";
+    public string IntEndpoint => "https://int.com";
+    public string ProdEndpoint => "https://prod.com";
+
+    public string MyData(string employeeId) => $"/api/v1/{employeeId}";
+    public string AllData => "/api/v1/";
+}
+
+public struct MyThirdService : IServiceWrapper
+{
+    public string DevEndpoint => "https://dev.com";
+    public string IntEndpoint => "https://int.com";
+    public string ProdEndpoint => "https://prod.com";
+
+    public string SomeDataEndpoint => $"Random Data End Point {Random.Shared.Next()}";
+}
+
+public interface IServiceWrapperFactory
+{
+    public ISplashTrackServicesWrapper<R> GetWrapperForType<R>() where R : IServiceWrapper, new();
+}
+
+public class ServiceWrapperFactory : IServiceWrapperFactory
+{
+    public ISplashTrackServicesWrapper<R> GetWrapperForType<R>() where R: IServiceWrapper, new() 
+        => new SplashTrackServicesWrapper<R>();
+}
+
+public interface ISplashTrackServicesWrapper<T> where T: IServiceWrapper, new()
+{
+    string HttpGet(Func<T, string> getEndpoint, bool logException = true, bool useRun = true);
+}
+
+public class SplashTrackServicesWrapper<T> : ISplashTrackServicesWrapper<T> where T: IServiceWrapper, new()
+{
+    protected string url;
+    private T _serviceWrapper;
+    public SplashTrackServicesWrapper()
+    {
+        var serviceWrapper = new T();
+        url = serviceWrapper.Endpoint;
+        _serviceWrapper = serviceWrapper;
+    }
+
+    public string HttpGet(Func<T,string> getEndpoint, bool logException = true, bool useRun = true)
+    {
+        return getEndpoint(_serviceWrapper);
+    }
+}
+
+
+public static class RunEnvironment
+{
+    public static string GetEnvironment() => "prod";
+}
